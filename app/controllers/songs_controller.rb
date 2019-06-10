750f677ee5849a29fd479a3973afd67a5cbb2af0
0506kinoshita <irohas.irohas@gmail.com>
2019-06-11 07:51:21 +0900

    調整

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,19 +1,20 @@
 class SongsController < ApplicationController
   def index
+    if user_signed_in?
+      @songs = Song.where(user_id: current_user.id)
+      else
+      @list = Song.all
+      end
     # user_signed_in?
-    @song=Song.new(
-      singer: song_params[:singer],
-      song: song_params[:song],
-      # user_id: current_user.id
-      )
-    @song.save
+    # @song=Song.new(
+    #   singer: song_params[:singer],
+    #   song: song_params[:song],
+    #   # user_id: current_user.id
+    #   )
+
     # @nickname=current_user.nickname
     # params.require(:item).permit(:image,).merge(user_id: current_user.id)
-    if user_signed_in?
-    @songs = Song.where(user_id: current_user.id)
-    else
-    @list = Song.all
-    end
+
 
   end
 
@@ -29,8 +30,7 @@ class SongsController < ApplicationController
       song: song_params[:song],
       user_id: current_user.id
       )
-    @song.save
-
+      @song.save
 
 
   end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
+  validates :singer, {presence: true}
+  validates :song, {presence: true}
+
   belongs_to :user
 end
